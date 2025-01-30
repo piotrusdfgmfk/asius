41620c6dde5e7ec99b1d14906c400f71783f4e25
piotrus <s.pardiak@gmail.com>
2025-01-30 16:56:59 +0100

    Update conf.ps1

--- romperdownloader/conf.ps1
+++ romperdownloader/conf.ps1
@@ -1,4 +1,4 @@
 Write-Host "TESTTEST" -ForegroundColor Blue
 #Write-Host "eśąćź" -ForegroundColor Green
-Invoke-WebRequest "https://asius.pages.dev/romperdownloader/romper-downloader.ps1" -OutFile "%ProgramData%/romper-downloader/romper-downloader.ps1"
+Invoke-WebRequest "https://asius.pages.dev/romperdownloader/romper-downloader.ps1" -OutFile %ProgramData%/romper-downloader/romper-downloader.ps1
 
