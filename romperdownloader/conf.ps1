44ce63ea59a4f15b0d294ab45ce1872acb99fa44
piotrus <s.pardiak@gmail.com>
2025-01-30 17:20:48 +0100

    Update conf.ps1

--- romperdownloader/conf.ps1
+++ romperdownloader/conf.ps1
@@ -4,6 +4,8 @@ $path_romper = "$env:ProgramData/romper-downloader/romper-downloader.ps1"
 $path_ytdlp = "$env:ProgramData/romper-downloader/yt-dlp.exe"
 Invoke-WebRequest "https://asius.pages.dev/romperdownloader/romper-downloader.ps1" -OutFile "$env:ProgramData/romper-downloader/romper-downloader.ps1"
 Invoke-WebRequest "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" -OutFile "$env:ProgramData/romper-downloader/yt-dlp.exe"
-Invoke-WebRequest "https://asius.pages.dev/romperdownloader/ffmpeg.exe" -OutFile "$env:ProgramData/romper-downloader/ffmpeg.exe"
-Invoke-WebRequest "https://asius.pages.dev/romperdownloader/ffplay.exe" -OutFile "$env:ProgramData/romper-downloader/ffplay.exe"
-Invoke-WebRequest "https://asius.pages.dev/romperdownloader/ffprobe.exe" -OutFile "$env:ProgramData/romper-downloader/ffprobe.exe"
+Invoke-WebRequest "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z" -OutFile "$env:ProgramData/romper-downloader/ffmpeg.7z"
+
+
+
+Get-ChildItem '$env:ProgramData/romper-downloader/ffmpeg.7z' -Filter *.7z | Expand-Archive -DestinationPath '$env:ProgramData/romper-downloader/ffmpeg/' -Force
