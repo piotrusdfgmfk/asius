279cc2e936e8c2ccfd7b0f58e20ee19e03ace442
piotrus <s.pardiak@gmail.com>
2025-01-30 17:15:59 +0100

    added ffmpeg exe files

--- romperdownloader/conf.ps1
+++ romperdownloader/conf.ps1
@@ -1,6 +1,9 @@
 Write-Host "TESTTEST" -ForegroundColor Blue
 #Write-Host "eśąćź" -ForegroundColor Green
-$path = "$env:ProgramData/romper-downloader/romper-downloader.ps1"
-Invoke-WebRequest "https://asius.pages.dev/romperdownloader/romper-downloader.ps1" -OutFile $path
-
-&./romper-downloader.ps1
+$path_romper = "$env:ProgramData/romper-downloader/romper-downloader.ps1"
+$path_ytdlp = "$env:ProgramData/romper-downloader/yt-dlp.exe"
+Invoke-WebRequest "https://asius.pages.dev/romperdownloader/romper-downloader.ps1" -OutFile "$env:ProgramData/romper-downloader/romper-downloader.ps1"
+Invoke-WebRequest "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" -OutFile "$env:ProgramData/romper-downloader/yt-dlp.exe"
+Invoke-WebRequest "https://asius.pages.dev/romperdownloader/ffmpeg.exe" -OutFile "$env:ProgramData/romper-downloader/ffmpeg.exe"
+Invoke-WebRequest "https://asius.pages.dev/romperdownloader/ffplay.exe" -OutFile "$env:ProgramData/romper-downloader/ffplay.exe"
+Invoke-WebRequest "https://asius.pages.dev/romperdownloader/ffprobe.exe" -OutFile "$env:ProgramData/romper-downloader/ffprobe.exe"
