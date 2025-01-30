28f83572292e48fd1c153431e2d9c44256c93d56
piotrus <s.pardiak@gmail.com>
2025-01-30 12:45:02 +0100

    Create download.bat

---
+++ romperdownloader/download.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:: Sprawdzenie, czy skrypt jest uruchomiony jako administrator
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Uruchom ten skrypt jako administrator.
+    pause
+    exit /b
+)
+
+:: Ustawienie ścieżki docelowej
+set "APP_FOLDER=%ProgramFiles%\romperdownloader"
+
+:: Tworzenie folderu docelowego (jeśli nie istnieje)
+if not exist "%APP_FOLDER%" mkdir "%APP_FOLDER%"
+
+:: Pobieranie skryptu PowerShell
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/piotrusdfgmfk/asius/blob/main/romperdownloader/romper-downloader.ps1' -OutFile '%APP_FOLDER%\romper-downloader.ps1'"
+
+:: Uruchamianie pobranego skryptu PowerShell
+powershell -ExecutionPolicy Bypass -File "%APP_FOLDER%\romper-downloader.ps1"
