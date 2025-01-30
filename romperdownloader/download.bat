7893fac837868221563e49fd1a096d29136ba83f
piotrus <s.pardiak@gmail.com>
2025-01-30 15:31:25 +0100

    Update download.bat

--- romperdownloader/download.bat
+++ romperdownloader/download.bat
@@ -4,13 +4,12 @@ setlocal EnableDelayedExpansion
 :: Sprawdzenie, czy skrypt jest uruchomiony jako administrator
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo Uruchom ten skrypt jako administrator.
-    pause
+    powershell -Command "Start-Process cmd -ArgumentList '/c "%~f0"' -Verb RunAs"
     exit /b
 )
 
 :: Ustawienie ścieżki docelowej
-set "APP_FOLDER=%ProgramFiles%\romperdownloader"
+set "APP_FOLDER=%ProgramFiles%\romper-downloader"
 
 :: Tworzenie folderu docelowego (jeśli nie istnieje)
 if not exist "%APP_FOLDER%" mkdir "%APP_FOLDER%"
