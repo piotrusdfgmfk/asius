8a8342f807bbf97f6630113fec7933acaad324f4
piotrus <s.pardiak@gmail.com>
2025-01-30 21:19:13 +0100

    0.97 final

--- romperdownloader/conf.ps1
+++ romperdownloader/conf.ps1
@@ -1,9 +1,7 @@
 function Sprawdzytdlp {
     try {
-        # Uruchomienie ffmpeg i pobranie pierwszej linii wyniku
         $output = & yt-dlp 2>&1 | Select-Object -First 2
 
-        # Sprawdzenie pierwszych 16 znaków
         if ($output -like "*System.Management.Automation.RemoteException*") {
             Write-Host "yt-dlp jest już zainstalowany"
         } 
@@ -21,16 +19,14 @@ Invoke-WebRequest "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-
     }
 }
 
-
  $path = "$env:ProgramData\romper-downloader"
-
 
 Write-Host "Czarodziej konfiguracji Romper Downloadera v0.97" 
 Write-Host "Pobieranie skryptów..."
 Invoke-WebRequest "https://asius.pages.dev/romperdownloader/romper-downloader.ps1" -OutFile "$env:ProgramData/romper-downloader/romper-downloader.ps1"
+
 Sprawdzytdlp
 
-# Sprawdzenie, czy ffmpeg jest zainstalowany
 $ffmpegInstalled = $false
 try {
     $ffmpegVersion = ffmpeg -version 2>$null
@@ -41,7 +37,6 @@ try {
     $ffmpegInstalled = $false
 }
 
-# Jeśli ffmpeg nie jest zainstalowany, wykonaj dodatkowy kod
 if (-not $ffmpegInstalled) {
     Write-Host "Pobieranie ffmpeg..."
     Invoke-WebRequest "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z" -OutFile "$env:ProgramData/romper-downloader/ffmpeg.7z"
@@ -50,12 +45,9 @@ if (-not $ffmpegInstalled) {
     $is_7zip = Test-Path "C:\PROGRA~1\7-Zip\7z.exe"
     if ($is_7zip)
     {
-        
         $7zipPath = "C:\PROGRA~1\7-Zip\7z.exe"
-    
         $zipFile = "$env:ProgramData/romper-downloader/ffmpeg.7z"
         $extractPath = "$env:ProgramData/romper-downloader/ffmpeg"
-        
         &"$7zipPath" e -o"$extractPath" -y "$zipFile" *> $null
     }
     else
@@ -68,10 +60,8 @@ if (-not $ffmpegInstalled) {
     $sourceFolder2 = "$env:ProgramData\romper-downloader\ffmpeg"
     $destinationPath = "$env:ProgramData\romper-downloader"
     
-    # Przeniesienie plików .exe bezpośrednio z folder2
     Get-ChildItem -Path $sourceFolder2 -Filter "*.exe" -File | Move-Item -Destination $destinationPath -Force
     
-    # Znalezienie folderu o dowolnej nazwie w folder2 i przeniesienie plików .exe z jego bin
     $binFolder = Get-ChildItem -Path $sourceFolder2 -Directory | ForEach-Object { "$($_.FullName)\bin" } | Where-Object { Test-Path $_ }
     
     if ($binFolder) {
@@ -83,7 +73,7 @@ if (-not $ffmpegInstalled) {
    Write-Host "ffmpeg jest już zainstalowany"
 }
 
-$folderToCheck = "$path\"  # Podmień na swój folder
+$folderToCheck = "$path\" 
 $pathEntries = [System.Environment]::GetEnvironmentVariable("Path", "Machine") -split ";" 
 
 if ($pathEntries -contains $folderToCheck) {
@@ -96,11 +86,11 @@ if ($pathEntries -contains $folderToCheck) {
     [EnvironmentVariableTarget]::Machine)
 }
 
+Invoke-WebRequest "https://asius.pages.dev/romperdownloader/install.bat" -OutFile "$env:ProgramData/romper-downloader/update_repair.bat"
+
 Write-Host "Instalacja zakończona pomyślnie"
 Write-Host " "
 Write-Host "Aby wywołać skrypt wpisz romper-downloader.ps1 w powershellu"
 Write-Host "Aby zaaktualizować lub naprawić skrypt i/lub jego zależności uruchom plik update_repair.bat w folderze $path"
-
-Invoke-WebRequest "https://asius.pages.dev/romperdownloader/install.bat" -OutFile "$env:ProgramData/romper-downloader/update_repair.bat"
 
 Pause
--- romperdownloader/install.bat
+++ romperdownloader/install.bat
@@ -1,33 +1,18 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Sprawdzenie, czy skrypt jest uruchomiony jako administrator
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     powershell -Command "Start-Process cmd -ArgumentList '/c "%~f0"' -Verb RunAs"
     exit /b
 )
 
-:: Ustawienie ścieżki docelowej
 set "APP_FOLDER=%ProgramData%\romper-downloader"
 
-:: Tworzenie folderu docelowego (jeśli nie istnieje)
 if not exist "%APP_FOLDER%" mkdir "%APP_FOLDER%"
 
-
-:: Pobieranie skryptu PowerShell
 powershell -Command "Invoke-WebRequest -Uri 'https://asius.pages.dev/romperdownloader/conf.ps1' -OutFile '%APP_FOLDER%\conf.ps1'"
-
-:: Konwersja pliku do UTF-8 bez BOM
-:: powershell -Command " $content = Get-Content '%APP_FOLDER%\conf.ps1' -Raw; $utf8NoBom = New-Object System.Text.UTF8Encoding $false; [System.IO.File]::WriteAllText('%APP_FOLDER%\conf.ps1', $content, $utf8NoBom) "
 
 :: dupadupa
 
-:: Uruchamianie pobranego skryptu PowerShell
 pwsh.exe -ExecutionPolicy Bypass -File "%APP_FOLDER%\conf.ps1"
-
-::pwsh.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%APP_FOLDER%\conf.ps1""' -Verb RunAs}"
-
-pause
-
-
